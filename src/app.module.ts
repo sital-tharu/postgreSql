91cf5184b98ee1002e8e15346b2823f4d8aae332
sital-tharu <sitatharu769@gmail.com>
2025-09-14 22:59:09 +0530

    creat a USerModuke and makes user entity's available to be used

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password', // <-- IMPORTANT: Change this to your database password
+      database: 'postgres',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // <-- Should be false in production
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
