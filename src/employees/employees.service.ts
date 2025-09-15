275a948bef58ae472541e490f9a043a4008e74c1
sital-tharu <sitatharu769@gmail.com>
2025-09-15 22:15:47 +0530

    dependency injection system to provide the service with a way to interact with the database

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Employees } from './employees.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class EmployeesService {}
+export class EmployeesService {
+    constructor (
+        @InjectRepository(Employees)
+        private employeesRepository: Repository<Employees>,
+    ){}
+}
