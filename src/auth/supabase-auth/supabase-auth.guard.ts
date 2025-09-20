ac20af36a7d0d9935dc38d10a6d2de2d78b19545
sital-tharu <sitatharu769@gmail.com>
2025-09-20 22:55:04 +0530

    Inject ConfigService into guard constructor for config access

--- src/auth/supabase-auth/supabase-auth.guard.ts
+++ src/auth/supabase-auth/supabase-auth.guard.ts
@@ -2,9 +2,11 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import * as jwt from 'jsonwebtoken';
 import{request} from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SupabaseAuthGuard implements CanActivate {
+  constructor(private configService: ConfigService) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
