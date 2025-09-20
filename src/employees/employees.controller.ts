713b351f0a8d936c23749d0a4b65a1572c00040e
sital-tharu <sitatharu769@gmail.com>
2025-09-20 23:30:14 +0530

    feat: impliment SupabaseAuthGuard in findAllEmployees route

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,20 +1,22 @@
-import { Controller, Post, Body, Get, Param, Put, Delete, Query } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Put, Delete, Query, UseGuards } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Employees } from './employees.entity';
+import { SupabaseAuthGuard } from 'src/auth/supabase-auth/supabase-auth.guard';
 
 @Controller('employees')
 export class EmployeesController {
-    constructor(private readonly employeesService: EmployeesService){}
+    constructor(private readonly employeesService: EmployeesService) { }
 
     @Post()
     async createEmployee(@Body() employeeData: Partial<Employees>): Promise<Employees> {
         return this.employeesService.create(employeeData);
     }
+    @UseGuards(SupabaseAuthGuard)
     @Get()
     async findAllEmployees(): Promise<Employees[]> {
         return this.employeesService.findAll();
     }
-        @Get('search')
+    @Get('search')
     async searchEmployees(
         @Query('name') Name?: string,
         @Query('department') department?: string,): Promise<Employees[]> {
@@ -22,18 +24,18 @@ export class EmployeesController {
 
     }
 
-    
+
 
     @Get(':id')
-    async findEmployeeById(@Param('id') id: number) : Promise<Employees> {
+    async findEmployeeById(@Param('id') id: number): Promise<Employees> {
         return this.employeesService.findOne(id);
     }
     @Put(':id')
-    async updateEmployee(@Param('id') id: number, @Body() Body :Partial <Employees>): Promise<Employees> {
-        return this.employeesService.update(id ,Body);
+    async updateEmployee(@Param('id') id: number, @Body() Body: Partial<Employees>): Promise<Employees> {
+        return this.employeesService.update(id, Body);
     }
     @Delete(':id')
-    async deleteEmployee(@Param('id') id: number): Promise<{message: string}> {
+    async deleteEmployee(@Param('id') id: number): Promise<{ message: string }> {
         return this.employeesService.delete(id);
     }
 }
